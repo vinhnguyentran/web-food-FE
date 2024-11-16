0ad8ff7129593e2d41d0d2f3dd9d57a3de5a3bc2
Trần Vinh Nguyễn <vinh.nguyentrandev@gmail.com>
2024-11-16 22:09:49 +0700

    pull

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  path: "/web-food-FE/",
+  base: "/web-food-FE/",
   plugins: [react()],
 })
